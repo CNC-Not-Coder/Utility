7a5c950e6884ad199a097c9b149f3d1677b839d3
CNC-Not-Coder <limotao@qq.com>
2016-01-08 21:01:33 +0800

    技能公共部分

---
+++ Common/Support/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyUtility
+{
+    public class Logger
+    {
+        public delegate void LogDelegate(string format, params object[] ps);
+        public static LogDelegate LogInfoHandler = null;
+        public static LogDelegate LogErrorHandler = null;
+
+        public static void Error(string format, params object[] ps)
+        {
+            if (LogErrorHandler != null) LogErrorHandler(format, ps);
+        }
+        public static void Info(string format, params object[] ps)
+        {
+            if (LogInfoHandler != null) LogInfoHandler(format, ps);
+        }
+    }
+}
